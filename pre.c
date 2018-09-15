a11e64b87fa128408872503c7c7e59b026ba3afc
Sam <samc@txstate.edu>
2018-09-15 10:23:35 -0500

    Got input working

--- pre.c
+++ pre.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char **argv){
 
-	//Check for at most 10 states in the list
-	//argc[0] = executable
-	//argc[1-21] = states and corresponding populations
-	if(argc > 21){
-		printf("There should be no more than 10 states entered \n");
-		return(-1);
+	//Variables
+	int i, j;
+	char List[1000];
+	char temp;
+
+		printf("Enter in a list of US states and their populations. Enter in the form: "
+				"TX 26 "
+				"NC 9 "
+				"MD 5 "
+				"NY 19 "
+				"Ctrl-D (to terminate the program. \n");
+	
+	while(scanf("%s %s", &List[i], &temp) != EOF){
+	//Read in input
+		i++;
 	}
-
-	//print out list
-	printf ("Number of inputs entered: %d \n", argc);
-	
-	for(int i = 0; i < argc; i++)
-		printf ("Input entered: %s \n", argv[i]);
-
-	
 
 
 
