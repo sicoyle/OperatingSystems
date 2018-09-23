ffcb5356252db29477e814d3fbb3b3de102bf05e
Sam <samc@txstate.edu>
2018-09-22 20:16:59 -0500

    Clean up code

--- sort.c
+++ sort.c
@@ -1,13 +1,18 @@
+/* Author: Samantha Coyle
+ * Date: 9/22/2018
+ * Purpose: This program takes in state abbreviations and sorts
+ * them into alphabetical order.
+*/
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 
 struct st_ex{
 	char name[3];
 //	int pop;
 };
 
+//Comparator function to compare state abbreviations
 int struct_cmp_by_state(const void *a, const void *b){
 
 	struct st_ex *ia = (struct st_ex *)a;
@@ -15,22 +20,8 @@ int struct_cmp_by_state(const void *a, const void *b){
 
 	return strcmp(ia -> name, ib -> name);
 }
-/*
-void print_struct_array(struct st_ex *array, size_t len){
-	size_t i;
-
-	for(i = 0; i < len; i++)
-		printf("%s\n", array[i].name);
-
-	printf("\n");
-}
-*/
-	
-
-
 
 int main(int argc, char **argv){
-
 	//no more than 10 states
 	if(argc > 11){
 		printf("%s\n", "Too many states entered");
@@ -38,17 +29,19 @@ int main(int argc, char **argv){
 	} else{	
 		struct st_ex structs[argc - 1]; 
 
+		//Copy over state abbreviations to the struct state name variable
 		for(int i = 1; i < argc; i++){
 			strcpy(structs[i].name, argv[i]);
 		}
 
 		size_t structs_len = sizeof(structs) / sizeof(struct st_ex) + 1;
 
+		//Sort struct state abbreviations
 		qsort(structs, structs_len, sizeof(struct st_ex), struct_cmp_by_state);
 		
+		//Print out sorted list
 		for(int i = 1; i < argc; i++)
 			printf("%s\n", &*structs[i].name);
-		
 	}
 
 	return 0;
