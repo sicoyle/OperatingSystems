7877b1695916510f6b3e531126c5642f11138c1a
Sam <samc@txstate.edu>
2018-09-22 20:52:58 -0500

    Got input working, need to get parent to read

--- homework1problem2.c
+++ homework1problem2.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#include <signal.h>
 int main(){
 	//variables to establish pipe
 	int p[2];
@@ -27,22 +27,26 @@ int main(){
 		//child
 		close(1);
 		dup(p[1]);
-		close(p[0]);
 		close(p[1]);
-
+		close(p[0]);
+		
 		//execute program
 		execl("./pre", "pre", (char *) 0);
-		return(EXIT_FAILURE);
+		//return(EXIT_FAILURE);
+
+		kill(pid, SIGTERM);
 	} else{
 		//parent
+		wait();
 		close(0);
+		open(1);//added
 		dup(p[0]);
 		close(p[0]);
 		close(p[1]);
 		
 		//execute program
 		execl("./sort", "sort", (char *) 0);
-		return(EXIT_FAILURE);
+		//return(EXIT_FAILURE);
 	}
 	
 
