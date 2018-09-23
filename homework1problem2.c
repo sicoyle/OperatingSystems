4c9c84ef800120a5d5b292b0c047726487760b61
Sam <samc@txstate.edu>
2018-09-22 20:36:17 -0500

    Working on getting pipe communication working

---
+++ homework1problem2.c
@@ -0,0 +1,54 @@
+/*Author: Samnatha Coyle
+ * Date: 9/22/2018
+ * Purpose: This program establishes a child to parent pipe.
+ * The child will execute the pre process and its output will connect to the piple
+ * to the parent, which will execute the sort process.
+ * */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+	//variables to establish pipe
+	int p[2];
+	int bytes;
+
+	//fork first so child can inherit pipe
+	pid_t pid = fork();
+
+	pipe(p);
+
+	//error state
+	if(pid < 0){
+		printf("There was an issue forking.\n");
+		exit(1);
+	} else if(pid == 0){
+		//child
+		close(1);
+		dup(p[1]);
+		close(p[0]);
+		close(p[1]);
+
+		//execute program
+		execl("./pre", "pre", (char *) 0);
+		return(EXIT_FAILURE);
+	} else{
+		//parent
+		close(0);
+		dup(p[0]);
+		close(p[0]);
+		close(p[1]);
+		
+		//execute program
+		execl("./sort", "sort", (char *) 0);
+		return(EXIT_FAILURE);
+	}
+	
+
+
+
+
+
+	return 0;
+}
