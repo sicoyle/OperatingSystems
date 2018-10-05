a8830ecef3fd276d910358ae7ef7b13088f82655
Sam <samc@txstate.edu>
2018-10-05 10:48:21 -0500

    Added partial code provided by professor

---
+++ project2/problem1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	char *path, *argv[20], buf[80], n, *p;
+
+	int m, status, inword, continu;
+
+	while(1) {
+		inword = 0;
+		p = buf;
+		m = 0;
+		continu = 0;
+
+		printf( "\nshhh> ");
+		while( ( n = getchar() ) != '\n' || continu) {
+			if( n == ' ' ) {
+				if (inword) {
+					inword = 0;
+					*p++ = 0;
+				}
+			}
+		        else if ( n == '\n' ) 
+				continu = 0;
+			else if ( n == '\\' && !inword ) 
+				continu = 1;
+			else {
+				if ( !inword ) {
+					inword = 1;
+					argv[m++] = p;
+					*p++ = n;
+				}
+				else 
+					*p++ = n;
+			}
+		}
+
+		*p++ = 0;
+		argv[m] = 0;
+
+		if( strcmp(argv[0],"exit") == 0) exit (0);
+
+		if( fork() == 0)
+		{
+			execvp( argv[0], argv);
+			printf ( "didn't exec \n ");
+		}
+
+		wait(&status);
+	}
+	return 0;
+}
+
+					
+
