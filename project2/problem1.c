cda55ff0a2593fa7261dab2af2680e0cf59b267f
Sam <samc@txstate.edu>
2018-10-05 11:03:44 -0500

    added skeleton for managing redirection

--- project2/problem1.c
+++ project2/problem1.c
@@ -7,6 +7,7 @@
 
 int main()
 {
+	//Given code for command parsing
 	char *path, *argv[20], buf[80], n, *p;
 
 	int m, status, inword, continu;
@@ -18,7 +19,7 @@ int main()
 		continu = 0;
 
 		printf( "\nshhh> ");
-		while( ( n = getchar() ) != '\n' || continu) {
+		while( ( n = getchar() ) != '\n' || continu ) {
 			if( n == ' ' ) {
 				if (inword) {
 					inword = 0;
@@ -43,16 +44,42 @@ int main()
 		*p++ = 0;
 		argv[m] = 0;
 
-		if( strcmp(argv[0],"exit") == 0) exit (0);
+		if( strcmp(argv[0],"exit") == 0 ) exit (0);
 
-		if( fork() == 0)
+		if( fork() == 0 )
 		{
-			execvp( argv[0], argv);
+			execvp( argv[0], argv );
 			printf ( "didn't exec \n ");
 		}
 
 		wait(&status);
 	}
+	//End of command parsing
+
+	int arguments[18] = {0};
+	int pipes = 0;
+	int pid = 0;
+
+	//Input redirection part
+	while(argv[count] != 0) {
+		if( strcmp( argv[count], "|") == 0) {
+			argv[count] = 0;
+			arguments[pipes + 1] = count + 1;
+			pipes++;
+		} else if( strcmp(argv[count], "<" ) == 0) {
+			//input redirection
+
+
+		} else if( strcmp(argv[count], ">" ) == 0) {
+			//output redirection
+
+		} else
+			arguments[count] = count;
+
+
+
+
+
 	return 0;
 }
 
