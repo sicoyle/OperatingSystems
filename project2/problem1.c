7d87e82f63ea303396e798e9143f96b3e387fc99
Sam <samc@txstate.edu>
2018-10-13 13:07:16 -0500

    problems with output rn

--- project2/problem1.c
+++ project2/problem1.c
@@ -4,6 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+
+#define READ 0
+#define WRITE 1
+
+
 
 int main()
 {
@@ -11,6 +17,9 @@ int main()
 	char *path, *argv[20], buf[80], n, *p;
 
 	int m, status, inword, continu;
+
+	char *inputPath;
+	char *outputPath;
 
 	while(1) {
 		inword = 0;
@@ -46,38 +55,128 @@ int main()
 
 		if( strcmp(argv[0],"exit") == 0 ) exit (0);
 
-		if( fork() == 0 )
+/*		if( fork() == 0 )
 		{
 			execvp( argv[0], argv );
 			printf ( "didn't exec \n ");
 		}
 
 		wait(&status);
+*/	
+		//End of command parsing
+
+		int count = 0;
+		int arguments[18] = {0};
+		int pipes = 0;
+		int pid = 0;
+		int inFlag = 0;
+		int outFlag = 0;
+		int rightPipe[2];
+		int leftPipe[2];
+
+		//Input redirection part
+		while(argv[count] != 0) {
+			if( strcmp( argv[count], "|") == 0) {
+				argv[count] = 0;
+				arguments[pipes + 1] = count + 1;
+				++pipes;
+			} else if( strcmp(argv[count], "<" ) == 0) {
+				//input redirection
+				inputPath = strdup(argv[count + 1]);
+				argv[count] = 0;
+				argv[count + 1] = 0;
+				inFlag = 1;
+			} else if( strcmp(argv[count], ">" ) == 0) {
+				//output redirection
+				outputPath = strdup(argv[count + 1]);
+				argv[count] = 0;
+				argv[count + 1] = 0;
+				outFlag = 1;
+			} else
+				arguments[count] = count;
+
+			++count;
+		}
+
+		for(int index = 0; index <= pipes; index++) {
+			if( (index != pipes) && (pipes > 0)) 
+				pipe(rightPipe);
+
+
+			switch (pid = fork()) {
+				case -1:
+					//error case
+					perror("The fork has failed!");
+					break;
+				case 0:
+					//children
+					if ((index == 0) && (inFlag == 1)){
+						int inFile = open(inputPath, O_RDONLY, 0400);
+						if(inFile == -1) {
+							perror("The input file has failed!\n");
+							return(EXIT_FAILURE);
+						}
+	
+						close(READ);
+						dup(inFile);
+						close(inFile);
+					}
+
+					if ((index == pipes) && (outFlag == 1)) {
+						int outFile = open(path, O_WRONLY | O_CREAT, 0755);
+						if(outFile < 0) {
+							perror("The ooutput file has failed!\n");
+							return(EXIT_FAILURE);
+						}
+						close(WRITE);
+						dup(outFile);
+						close(outFile);
+					}
+
+					if (pipes > 0) {
+						//1st child process
+						if (index == 0) {
+							close(WRITE);
+							dup(rightPipe[WRITE]);
+							close(rightPipe[WRITE]);
+							close(rightPipe[READ]);
+						} else if ( index < pipes){
+							//2nd child process
+							close(READ);
+							dup(leftPipe[READ]);
+							close(leftPipe[READ]);
+							close(leftPipe[WRITE]);
+							close(WRITE);
+							dup(rightPipe[WRITE]);
+							close(rightPipe[READ]);
+							close(rightPipe[WRITE]);
+						} else{
+							//last child process
+							close(READ);
+							dup(leftPipe[READ]);
+							close(leftPipe[READ]);
+							close(leftPipe[WRITE]);
+						}
+					}
+					execvp(argv[arguments[index]], &argv[arguments[index]]);
+					perror("The command failed to execute!\n");
+					break;
+				default:
+					//parent process
+					if (index > 0) {
+						close(leftPipe[READ]);
+						close(leftPipe[WRITE]);
+					}
+	
+					leftPipe[READ] = rightPipe[READ];
+					leftPipe[WRITE] = rightPipe[WRITE];
+	
+					wait(&status);
+					break;
+			}
+		}
+
 	}
-	//End of command parsing
-
-	int arguments[18] = {0};
-	int pipes = 0;
-	int pid = 0;
-
-	//Input redirection part
-	while(argv[count] != 0) {
-		if( strcmp( argv[count], "|") == 0) {
-			argv[count] = 0;
-			arguments[pipes + 1] = count + 1;
-			pipes++;
-		} else if( strcmp(argv[count], "<" ) == 0) {
-			//input redirection
-
-
-		} else if( strcmp(argv[count], ">" ) == 0) {
-			//output redirection
-
-		} else
-			arguments[count] = count;
-
-
-
 
 
 	return 0;
