1c4f3c46d2e6fd19e19f2a325f5906968acd1e63
Sam <samc@txstate.edu>
2018-11-04 08:38:47 -0600

    Create threads

--- project3/assignment1.c
+++ project3/assignment1.c
@@ -16,8 +16,21 @@ pthread_mutex_t mutex;
 struct Award award;
 
 int main() {
+	//Initial values
+	award.totalAwardAmount = 4000.0;
+	award.temp = 0.0;
 
+	//Thread ids and attributes
+	pthread_t tid1, tid2, tid3;
+	pthread_attr_t attribute;
 
+	printf("First person takes away $ %d \n", award.totalAwardAmount);
+	printf("First person takes away $ %d \n", award.temp);
+
+	//Create threads
+	pthread_create(&tid1, NULL, (void *(*)(void*))takeAwardFirst, NULL);
+	pthread_create(&tid2, NULL, (void *(*)(void*))takeAwardSecond, NULL);
+	pthread_create(&tid3, NULL, (void *(*)(void*))takeAwardThird, NULL);
 
 	pthread_exit(0);
 }
